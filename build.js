621e5ce33a54a93d15887d7e3cb3d9640167c505
xiaoqiang1999 <1362061285@qq.com>
2021-06-05 17:27:23 +0800

    correct dist-build babel

--- build.js
+++ build.js
@@ -7,14 +7,14 @@ const shell = require('shelljs');
 shell.rm('-rf', './build');
 
 shell.exec('tsc --project ./tsconfig.prod.json');
-shell.exec('babel ./dist --out-dir ./dist');
+shell.exec('babel ./build --out-dir ./build');
 
-shell.cp('-r', './src/static', './dist/');
+shell.cp('-r', './src/static', './build/');
 
 console.log('build Success');
 
 // const babel = require('@babel/core');
-// babel.transformFileSync('./dist/index.js', {
+// babel.transformFileSync('./build/index.js', {
 // 	configFile: './.babelrc',
 // });
 
@@ -23,6 +23,6 @@ console.log('build Success');
 // const tscCommand = resolve('./node_modules/.bin/tsc');
 // const babelCommand = resolve('./node_modules/.bin/babel');
 // execSync(`${tscCommand} --project ./tsconfig.prod.json`);
-// execSync(`${babelCommand} ./dist --out-dir ./dist`);
+// execSync(`${babelCommand} ./build --out-dir ./build`);
 
 // console.log(JSON.parse(process.env.npm_config_HOST));
