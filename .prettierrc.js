22f91a8b3291099b7203ae5da30b995048dacd16
xiaoqiang1999 <1362061285@qq.com>
2021-06-06 19:30:53 +0800

    cli fix

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
 	useTabs: true, // 缩进使用 tab
-	tabWidth: 2,
+	tabWidth: 2, // tab缩进大小 默认为2
 	semi: true, // 句尾添加分号
-	singleQuote: true,
-	arrowParens: 'avoid',
-	printWidth: 90,
+	singleQuote: true, // 字符串使用单引号
+	arrowParens: 'avoid', // 能省略括号的时候就省略 例如 x => y
+	printWidth: 80, // 换行长度，默认80
 };
