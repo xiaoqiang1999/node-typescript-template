ed835dbedf92f1f0961b0f9861ca79c7469cdfb2
xiaoqiang1999 <1362061285@qq.com>
2021-06-06 19:00:15 +0800

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,21 @@
 module.exports = {
-	parser: '@typescript-eslint/parser', //定义ESLint的解析器
-	plugins: ['@typescript-eslint/eslint-plugin', 'eslint-plugin-prettier'], //定义了该eslint文件所依赖的插件 插件决定lint规则
-	extends: ['plugin:prettier/recommended'], //定义文件继承的子规范 代码规范
+	root: true,
+	// 定义ESLint的解析器
+	parser: '@typescript-eslint/parser',
+	// 定义eslint要加载的的插件
+	plugins: ['@typescript-eslint', 'eslint-plugin-prettier'],
+	// 扩展eslint配置 (代码规范 lint规则)
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'prettier', // 启用eslint-config-prettier 关闭与prettier冲突的eslint规则
+	],
+	rules: {
+		// 启用eslint-plugin-prettier提供的规则 并使用error级别的错误(错误级别可自定义)
+		'prettier/prettier': 'error',
+		// prettier 建议关闭的配置 不关闭可能会导致某些情况下出现问题 请参阅eslint-plugin-prettier的GitHub文档
+		'arrow-body-style': 'off',
+		'prefer-arrow-callback': 'off',
+	},
 	env: {
 		//指定代码的运行环境
 		node: true,
